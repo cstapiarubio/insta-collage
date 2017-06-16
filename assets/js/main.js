dbc80951623626e6c7447b8151fb8013b661b63e
cstapiarubio <cstapiarubio@gmail.com>
2017-06-15 22:41:30 -0400

    haciendo las validaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-main.js
+function validación(valor){
+	var valor = document.getElementsByClassName("form-control").value;
+	if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+		alert ("Debe rellenar el recuadro");
+	}
+}
+validación();
