6271553e99c2b08df7405f0f7cca2df62b658598
cstapiarubio <cstapiarubio@gmail.com>
2017-06-16 18:14:23 -0400

    tratando de hacer la funcionalidaddrag and drop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+/*función vaidación de campos*/
 function validacion(){
 
 	var nombre = document.getElementById("usr").value;
@@ -17,4 +18,25 @@ function validacion(){
 	}
 }
 validacion();
+
+/*función drag and drop collage*/
+
+function drag(ev){
+	console.log(ev.target.id);
+	ev.dataTransfer.setData("text", ev.target.id);
+}
+drag();
+
+function permitirDrop(ev){
+	ev.preventDefault();
+}
+permitirDrop();
+
+function drop(ev){
+	ev.preventDefault();
+	var id_foto=ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(id_foto));
+}
+drop();
+
 
